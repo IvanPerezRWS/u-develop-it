647f4f6a6ed1e33a6919bdc2f2047e74149a989c
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-07-22 16:06:22 -0500

    Connected database

--- server.js/server.js
+++ server.js/server.js
@@ -1,4 +1,5 @@
 
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,27 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// GET test route 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello world!'
+    });
+});
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host = 'localhost',
+        // Your MYSQL username,
+        user: 'root',
+        // Your MYSQL password
+        password: 'yonaguni',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
